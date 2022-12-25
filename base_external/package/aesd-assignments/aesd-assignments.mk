ee7b2a8b4e17c780528ca503bd1b1fa259c3d196
sachinkp21587 <sachinkp21587@gmail.com>
2022-12-25 11:29:53 +0530

    readded cross compile to check

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -16,7 +16,7 @@ AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 CROSS_COMPILE=aarch64-none-linux-gnu-
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
-$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app CROSS_COMPILE=$(CROSS_COMPILE) all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
